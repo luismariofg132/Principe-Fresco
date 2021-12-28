2f6d75551e3f6155df274c2db5bfa949a8cc195e
Luis Mario <lmfg06@gmail.com>
2021-12-27 20:13:49 -0500

    Estructura de carpetas

---
+++ js/snowstorm-min.js
@@ -0,0 +1,9 @@
+// 1. Define un color para la nieve
+snowStorm.snowColor = '#ccff00';
+// 2. Para optimizar, defina el número máximo de copos que pueden
+// aparecer en la pantalla a la vez
+snowStorm.flakesMaxActive = 30;
+// 3. Deje que la nieve entre y salga de la vista
+snowStorm.useTwinkleEffect = true; 
+// 4. ¡Empieza la tormenta de nieve!
+snowStorm.start();
