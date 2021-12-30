34ce6b7f7be812c787681820067ce467fbeacd19
Luis Mario <lmfg06@gmail.com>
2021-12-30 12:43:33 -0500

    Consumo de la API

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
+const API_PRODUTS = "https://luismariofg132.github.io/Principe-Fresco/data/dbProductos.json"
+const box_container = document.getElementById('box-container')
 
 
-document.addEventListener('DOMContentLoaded', () => {
+const getProduts = (produts) => {
+    const peticion = fetch(produts);
+    peticion
+        .then((resp) => resp.json())
+        .then((data) => showProduts(data.produts))
+        .catch(err => console.log(err))
+}
 
-})
+getProduts(API_PRODUTS);
+
+const showProduts = (produts) => {
+    box_container.innerHTML = ''
+    produts.forEach(element => {
+        const { id, nombre, precio, precioInicial, descuento, image } = element
+        const box = document.createElement('div')
+        box.classList.add('box');
+        box.innerHTML = `
+        <span class="discount">${descuento}</span>
+                <div class="icons">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-share"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                </div>
+                <img src="${image}"
+                    alt="">
+                <h3>${nombre}</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price"> ${precio} <span> ${precioInicial} </span> </div>
+                <a href="#" class="btn">add to cart</a>
+        `
+        box_container.appendChild(box)
+
+    });
+}
